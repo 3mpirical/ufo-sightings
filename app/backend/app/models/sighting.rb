da279c5debdcbbd6b1b8655c7b6e7523178ee51a
nick ristagno <3mpirical10510@gmail.com>
2020-10-08 20:39:18 -0600

    added filters and pagination

--- app/backend/app/models/sighting.rb
+++ app/backend/app/models/sighting.rb
@@ -1,5 +1,32 @@
 class Sighting < ApplicationRecord
   include Locatable
+  include Filterable
+  include Orderable
+
+  # FILTERS
+  filterable_by :shape, ->(shape) { where(shape: shape) }
+  filterable_by :city, ->(city) { where(city: city) }
+  filterable_by :state, ->(state) { where(state: state) }
+  filterable_by :duration_seconds, ->(duration_seconds) {
+    where(duration_seconds: duration_seconds)
+  }
+
+  # Gets all sightings within the radius of the given longitude and latitude
+  filterable_by_with_params :radius, ->(radius, params) {
+    within(params[:longitude], params[:latitude], radius)
+  }
+
+  # Data to be used in location search and nearest ordering
+  filterable_data :longitude
+  filterable_data :latitude
+
+  orderable_by(
+    :created_at,
+    :updated_at,
+    :duration_seconds,
+    :shape,
+    :sighting_date,
+  )
 
   # VALIDATIONS
   validates :shape, length: { maximum: 500, message: "Shape cannot be more than 500 characters" }
