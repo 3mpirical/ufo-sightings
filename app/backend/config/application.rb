b2b4404c015ea2c11c3d0484c4669f4a619bc5ab
nick ristagno <3mpirical10510@gmail.com>
2020-10-08 18:22:31 -0600

    added auto/eager loading for lib directory

--- app/backend/config/application.rb
+++ app/backend/config/application.rb
@@ -24,6 +24,10 @@ module Backend
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # config.middleware.use Rack::Attack
+
+    config.autoload_paths << Rails.root.join("lib")
+    config.eager_load_paths << Rails.root.join("lib")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
